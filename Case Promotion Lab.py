25acdcd7f1ad0583ce3b9899d0e2e731c75ce1d8
admin <root@localhost>
2022-10-14 00:37:43 +0000

    Lab 5, step 5

---
+++ Case Promotion Lab.py
@@ -0,0 +1,102 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+@phantom.playbook_block()
+def on_start(container):
+    phantom.debug('on_start() called')
+
+    # call 'format_1' block
+    format_1(container=container)
+
+    return
+
+@phantom.playbook_block()
+def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("format_1() called")
+
+    template = """A file has been detected with potentially malicious content. A case has been opened.\n\n-**Case Link**:{0}\n-**Container Name**: {1}\n-**Container Description**: {2}\n-**Source URL**: {3}\n-**Target server IP**: {4}\n-**Suspicious File Path**: {5}\n-**Reason for promotion**: {6}\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "container:url",
+        "container:name",
+        "container:description",
+        "artifact:*.cef.sourceDnsDomain",
+        "artifact:*.cef.destinationAddress",
+        "artifact:*.cef.filePath",
+        "playbook_input:promotion_reason"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_1", drop_none=True)
+
+    add_comment_promote_to_case_add_note_1(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def add_comment_promote_to_case_add_note_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("add_comment_promote_to_case_add_note_1() called")
+
+    format_1 = phantom.get_format_data(name="format_1")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment="Promoted to case")
+    phantom.promote(container=container, template="Data Breach")
+    phantom.add_note(container=container, content=format_1, note_format="markdown", note_type="general", title="Incident Report")
+
+    container = phantom.get_container(container.get('id', None))
+
+    return
+
+
+@phantom.playbook_block()
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # This function is called after all actions are completed.
+    # summary of all the action and/or all details of actions
+    # can be collected here.
+
+    # summary_json = phantom.get_summary()
+    # if 'result' in summary_json:
+        # for action_result in summary_json['result']:
+            # if 'action_run_id' in action_result:
+                # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
+                # phantom.debug(action_results)
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
